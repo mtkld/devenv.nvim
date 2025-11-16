8c7bf3556f881162b7dcede5c09191db4aebff76
mtkld <194388525+mtkld@users.noreply.github.com>
2025-11-16 06:48:49 +0100

    General update]

--- lua/devenv/init.lua
+++ lua/devenv/init.lua
@@ -1,12 +1,22 @@
 local M = {}
 
-local default_opts = {}
+local default_opts = { enable_wswrite_log = true }
 
 function M.setup(opts)
 	opts = vim.tbl_deep_extend("force", default_opts, opts or {})
 
-	--_G.log = require("wswrite").log
-	_G.log = require("nntmwrite").log
+	if opts.enable_wswrite_log == nil or opts.enable_wswrite_log then
+		-- If wswrite is enabled, then we use it to log.
+		_G.log = require("wswrite").log
+		--_G.log = require("nntmwrite").log
+	else
+		-- If wswrite is not enabled, then we use the default log.
+		-- just use print
+		_G.log = function(...)
+			-- Do nothing... prints will interrupt the flow of the program
+			--print(...)
+		end
+	end
 end
 
 return M
